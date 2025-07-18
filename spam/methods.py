267b41c9cfd67aee9dbfff79d1124d5c54adeb43
Кузнецов Максим <m.kuznetsov@aitiguru.com>
2025-07-18 18:01:48 +0300

    fix query users

--- spam/methods.py
+++ spam/methods.py
@@ -2,19 +2,19 @@ from connect import bot, logging, db
 from psycopg2.extras import DictCursor
 from telebot import types
 
+from tables import User
 
 
-def spamMessage(message: types.Message, execute: str, inlineKeyboard: types.InlineKeyboardButton | None = None) -> None:
-    with db.cursor(cursor_factory=DictCursor) as cursor:
-        cursor.execute(execute)
-        for item in cursor.fetchall():
-            try:
-                bot.copy_message(
-                    chat_id=item["telegram_id"],
-                    from_chat_id=message.reply_to_message.chat.id,
-                    message_id=message.reply_to_message.id,
-                    disable_notification=False,
-                    reply_markup=inlineKeyboard
-                )
-            except Exception:
-                logging.error("Не удалось отправить spam сообщение пользователю")
+def spamMessage(message: types.Message, users: set[User], inlineKeyboard: types.InlineKeyboardButton | None = None) -> None:
+
+    for user in users:
+        try:
+            bot.copy_message(
+                chat_id=user.telegram_id,
+                from_chat_id=message.reply_to_message.chat.id,
+                message_id=message.reply_to_message.id,
+                disable_notification=False,
+                reply_markup=inlineKeyboard
+            )
+        except Exception:
+            logging.error("Не удалось отправить spam сообщение пользователю")
