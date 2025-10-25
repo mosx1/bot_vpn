c1a2b79310f33c699bbc684a8e5f456559f2e3ff
Кузнецов Максим <m.kuznetsov@aitiguru.com>
2025-10-25 13:27:16 +0300

    add users to subscriptions

--- tables.py
+++ tables.py
@@ -62,6 +62,20 @@ class GiftCodes(Base):
 
     code: Column[str] = Column(TEXT, primary_key=True)
     month: Column[int] = Column(SMALLINT, nullable=False)
+
+
+class UserToSubscription(Base):
+
+    __tablename__: str = 'user_to_subscription'
+
+    telegram_id: Column = Column(BIGINT, nullable=False)
+    server_link: Column = Column(TEXT, primary_key=True)
+    server_id: Column = Column(BIGINT, nullable=False)
+
+    __table_agrs___ = (
+        ForeignKeyConstraint(['telegram_id'], ['users_subscription.telegram_id']),
+        ForeignKeyConstraint(['server_id'], ['servers.id'])
+    )
     
     
 Base.metadata.create_all(engine)
